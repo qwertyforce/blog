5da211c4b749bd93da7f40869f30e211c0d43372
qwertyforce <44163887+qwertyforce@users.noreply.github.com>
2020-08-11 19:35:35 +0300

    add date

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -2,24 +2,29 @@ import { getAllPostIds, getPostData } from '../../helpers/posts'
 import Head from 'next/head'
 import utilStyles from '../../styles/utils.module.css'
 import Link from 'next/link'
+import Date from '../../components/Date'
 
 export default function Post({ postData }) {
   return (
     <div>
-      <div className={utilStyles.back_button}>
-      <Link href="/">
-      <a className="noDecoration"><p className={utilStyles.back_button_background}>back</p></a>
-          </Link>
-      </div>
-    <div className="container">
       <Head>
         <title>{postData.title}</title>
       </Head>
+      <div className={utilStyles.back_button}>
+      <Link href="/">
+      <a className="noDecoration">
+        <p className={utilStyles.back_button_background}>back</p>
+        </a>
+          </Link>
+      </div>
+    <div className="container">
       <article>
-        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+       <div>
+       <h1 className={utilStyles.headingXl}>{postData.title}</h1>
         <div className={utilStyles.lightText}>
-          {/* <Date dateString={postData.date} /> */}
-        </div>
+          <Date dateString={postData.date} />
+        </div></div> 
+        
         <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
       </article>
     </div>
