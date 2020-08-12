22e00dde5d1e33d334c59b8d8ec13350ed9243e9
qwertyforce <44163887+qwertyforce@users.noreply.github.com>
2020-08-12 14:38:30 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -30,7 +30,6 @@ export default function Home({ allPostsData }) {
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData()
-  console.log(allPostsData)
   return {
     props: {
       allPostsData
